6cb897f747a3a2a5d6d4814f60f53a185c342374
Darkshadow229761 <aayushmaanshah700@gmail.com>
2026-09-23 19:48:52 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import { initializePWA } from './lib/pwa';
-import './style/main.css';
-import AppErrorBoundary from './components/AppErrorBoundary';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { initializePWA } from "./lib/pwa";
+import AppErrorBoundary from "./components/AppErrorBoundary";
+import "./style/main.css";
 
 initializePWA();
 
-createRoot(document.getElementById('root')!).render(
-  <AppErrorBoundary>
-    <React.StrictMode>
+const root = document.getElementById("root");
+
+if (!root) {
+  throw new Error("NEXUS could not find the root element.");
+}
+
+createRoot(root).render(
+  <React.StrictMode>
+    <AppErrorBoundary>
       <App />
-    </React.StrictMode>
-  </AppErrorBoundary>,
+    </AppErrorBoundary>
+  </React.StrictMode>
 );
